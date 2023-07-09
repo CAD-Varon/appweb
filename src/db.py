b9905516c0ce616615d2c1c036026b062ca8dc69
CAD-Varon <137358969+CAD-Varon@users.noreply.github.com>
2023-07-09 09:45:07 -0500

    Add files via upload

---
+++ src/db.py
@@ -0,0 +1,5 @@
+import pymysql
+
+# mysql = pymysql.connect(host='bvy1i4ievctgaezykv3i-mysql.services.clever-cloud.com', port=3306, user='uvnkiszigk5xyif3', passwd='0mrtxQFMXfR3FsFzvk04', database='bvy1i4ievctgaezykv3i')
+
+mysql = pymysql.connect(host='bvy1i4ievctgaezykv3i-mysql.services.clever-cloud.com', port=3306, user='uvnkiszigk5xyif3', passwd='0mrtxQFMXfR3FsFzvk04', database='bvy1i4ievctgaezykv3i')
